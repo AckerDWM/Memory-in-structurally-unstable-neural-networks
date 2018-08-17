9abe5317940c2f010b67fd75f57815add49099db
Alizaybak <danielacker@Daniels-MacBook-Pro-4.local>
2018-08-17 15:17:48 -0700

    cleaning and annotation

--- simulation_code/orientation_tuning/imports.R
+++ simulation_code/orientation_tuning/imports.R
@@ -6,8 +6,13 @@ np = reticulate::import("numpy")
 theme_set(
   theme_classic() +
     theme(
-      text = element_text(size=8),
-      strip.background = element_blank(),
-      legend.margin = margin(0, 0, 0, 0)
-      )
-  )
+      text=element_text(size=8, family="Times New Roman"),
+      legend.background=element_rect(fill=NA),
+      legend.margin = margin(0, 0, 0, 0),
+      axis.line = element_line(size=.25),
+      axis.ticks = element_blank(),
+      axis.text.y = element_text(margin = margin(r = -1)),
+      axis.text.x = element_text(margin = margin(t = -1)),
+      strip.background = element_rect(fill=NA, color=NA)
+    )
+)
